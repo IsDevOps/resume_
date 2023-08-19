69be82eaa38e078b433141c5a964fe3acf304444
IsDevOp <issyoseghale.com>
2023-08-19 21:57:43 +0100

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ class About extends Component {
                     <br />
                     <span>{email}</span>
                   </p>
-                </div>
+                </div> 
                 <div className="columns download">
                   <p>
                     <a href={resumeDownload} className="button">
